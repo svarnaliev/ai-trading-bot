a404ae6211002530b37516911ed5b5db164cfc8a
svarnaliev <svarnaliev@gmail.com>
2026-03-07 19:52:07 +0300

    main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ INTERVAL_SECONDS = 900
 MODEL_FILE = 'catboost_model.cbm'
 
 MIN_DATA_LENGTH = 50
-PROBABILITY_THRESHOLD = 0.25
+PROBABILITY_THRESHOLD = 0.20
 
 FEATURES = ['ema200', 'rsi', 'macd', 'bb_lower', 'price_change', 'volume_change']
 
